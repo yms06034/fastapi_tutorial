304202ef5b4750227628c643b1a74732499e8611
yms06034 <yms06034@gmail.com>
2023-10-17 17:41:26 +0900

    send

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import uvicorn
+from typing import Optional
 from fastapi import FastAPI, Request
 
 app = FastAPI()
@@ -17,6 +18,18 @@ def get_user(user_id:int, request: Request):
     print(request.path_params)
     return {"user_id" : user_id}
 
+
+# @app.get("/users")
+# def get_users(limit: Optional[int] = None):
+#     return {"limit" : limit}
+
+
+@app.get("/users")
+def get_users(is_admin: bool, limit: int = 100):
+    """
+    pydantic이 (True, False), (true, false), (1, 0), (yes, no)을 알아서 판단 후 bool을 처리
+    """
+    return {"is_admin":is_admin, "limit":limit}
 
 
 if __name__ == "__main__":
